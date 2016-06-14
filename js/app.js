4c87e358b78180769f6966523545cff8d4e73e7f
Robby Kim <robert.st.kim@gmail.com>
2016-06-14 15:53:23 -0400

    finished graying/deleting portion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
-	//add items by clicking on the cart button
+	// add items by clicking on the cart button
 	$('#item-submit').on('click', function(){
-		var item = $('#area').val();
-		var text ='<li class="active-itmes"><span>'+item+'</span> <i class="fa fa-check-square" aria-hidden="true"></i> <i class="fa fa-trash" aria-hidden="true"></i></li>'
+		var item = $('.add-items').val();
+		var text ='<li class="active-items"><span>'+item+'</span> <i class="fa fa-check-square" aria-hidden="true"></i> <i class="fa fa-trash" aria-hidden="true"></i></li>'
 		$('.items-list').append(text);
 	});
+
+	// checking off bought items
+ 	$('.items-list').on('click', '.fa-check-square', function(){
+ 		$(this).closest('.active-items').css('color', 'lightgray').toggleClass('active-items inactive-items');
+ 	});
+
+ 	// $('.inactive-items').mouseenter(function() {
+ 	// 	$('inactive-items').closest('.fa-trash').css('color', 'black');
+ 	// })
+
+ 	// removes items by clicking the trash icon
+ 	$('.items-list').on('click', '.fa-trash', function () {
+ 		$(this).closest('li').remove();
+ 	});
 });
