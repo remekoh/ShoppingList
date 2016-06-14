b2252923756f888bdbfdc31e1b5bc88257d5fb30
rem{e}koh <git@remekoh.me>
2016-06-14 15:14:39 -0500

    JavaScript Part 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+	//add items with enter button
+	$(document).keyup(function(event){
+		if(event.keyCode == 13){
+    		var item = $('.add-items').val();
+			var text ='<li class="active-items"><span>'+item+'</span> <i class="fa fa-check-square" aria-hidden="true"></i> <i class="fa fa-trash" aria-hidden="true"></i></li>'
+			$('.items-list').append(text);
+			$('.add-items').val('');
+    }
+	});
+
 	// add items by clicking on the cart button
 	$('#item-submit').on('click', function(){
 		var item = $('.add-items').val();
 		var text ='<li class="active-items"><span>'+item+'</span> <i class="fa fa-check-square" aria-hidden="true"></i> <i class="fa fa-trash" aria-hidden="true"></i></li>'
 		$('.items-list').append(text);
+		$('.add-items').val('');
 	});
 
 	// checking off bought items
